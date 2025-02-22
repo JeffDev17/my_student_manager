de63b6ef63959fa9423845f82f12c61c8c001ed1
JeffDev17 <jeeff.araujo1@gmail.com>
2025-02-22 18:13:12 -0300

    navbar, first model and views fully working

--- students/models.py
+++ students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Ativo'),
+        ('inactive', 'Inativo'),
+    ]
+
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    package = models.IntegerField(choices=[(4, '4 aulas'), (8, '8 aulas'), (12, '12 aulas'), (16, '16 aulas'), (20, '20 aulas')])
+    payment_day = models.IntegerField()
+    phone_number = models.CharField(max_length=15)
+    accumulated_classes = models.IntegerField(default=0)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        return self.name
